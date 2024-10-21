7c7863a559f69fd22cf5118e0c39a9df899a2ff3
kravchenko-d <kravchenko4@mail.ru>
2024-10-21 09:55:57 +0300

    feat: input validation with regex

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import { validateIp } from './helpers';
+
 const ipInput = document.querySelector('.search-bar__input');
 const btn = document.querySelector('button');
 
@@ -5,14 +7,17 @@ btn.addEventListener('click', getData);
 ipInput.addEventListener('keydown', handleKey);
 
 function getData() {
-    // проверка данных
-    fetch(`https://geo.ipify.org/api/v2/country,city?apiKey=at_cIbBw5cHPNoqugE7p208zyb3y0bH9&ipAddress=${ipInput.value}`)
-    .then(response => response.json())
-    .then(console.log);
+  if (validateIp(ipInput.value)) {
+    fetch(
+      `https://geo.ipify.org/api/v2/country,city?apiKey=at_cIbBw5cHPNoqugE7p208zyb3y0bH9&ipAddress=${ipInput.value}`,
+    )
+      .then((response) => response.json())
+      .then(console.log);
+  }
 }
 
 function handleKey(e) {
-    if (e.key === 'Enter') {
-        getData();
-    }
+  if (e.key === 'Enter') {
+    getData();
+  }
 }
