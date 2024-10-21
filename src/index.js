e0ccf62065b09f5287090d6662050c5f2bc5fffc
kravchenko-d <kravchenko4@mail.ru>
2024-10-21 10:07:26 +0300

    feat: render dom elements

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import { validateIp } from './helpers';
 const ipInput = document.querySelector('.search-bar__input');
 const btn = document.querySelector('button');
 
+const inInfo = document.querySelector('#ip');
+const locationInfo = document.querySelector('#location');
+const timezoneInfo = document.querySelector('#timezone');
+const ispInfo = document.querySelector('#isp');
+
 btn.addEventListener('click', getData);
 ipInput.addEventListener('keydown', handleKey);
 
@@ -12,7 +17,7 @@ function getData() {
       `https://geo.ipify.org/api/v2/country,city?apiKey=at_cIbBw5cHPNoqugE7p208zyb3y0bH9&ipAddress=${ipInput.value}`,
     )
       .then((response) => response.json())
-      .then(console.log);
+      .then(setInfo);
   }
 }
 
@@ -20,4 +25,12 @@ function handleKey(e) {
   if (e.key === 'Enter') {
     getData();
   }
+}
+
+function setInfo(mapData) {
+    console.log(mapData);
+    inInfo.innerText = mapData.ip;
+    locationInfo.innerText = mapData.location.country + ', ' + mapData.location.region;
+    timezoneInfo.innerText = mapData.location.timezone;
+    ispInfo.innerText = mapData.isp;
 }
