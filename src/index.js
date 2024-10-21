8407e0ff17d9ef74f6ae42661a848740ca313f4a
kravchenko-d <kravchenko4@mail.ru>
2024-10-21 09:39:36 +0300

    feat: handle user input

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-console.log('hello from js');
+const ipInput = document.querySelector('.search-bar__input');
+const btn = document.querySelector('button');
+
+btn.addEventListener('click', getData);
+ipInput.addEventListener('keydown', handleKey);
+
+function getData() {
+    // проверка данных
+    fetch(`https://geo.ipify.org/api/v2/country,city?apiKey=at_cIbBw5cHPNoqugE7p208zyb3y0bH9&ipAddress=${ipInput.value}`)
+    .then(response => response.json())
+    .then(console.log);
+}
+
+function handleKey(e) {
+    if (e.key === 'Enter') {
+        getData();
+    }
+}
