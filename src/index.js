0e689ae69c2027fbeefeeeba1b30f70b99d6a9a7
kravchenko-d <kravchenko4@mail.ru>
2024-10-21 10:44:35 +0300

    feat: add map marker

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
-import { validateIp } from './helpers';
+import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
+import { addTileLayer, validateIp } from './helpers';
+import icon from '../images/icon-location.svg';
 
 const ipInput = document.querySelector('.search-bar__input');
 const btn = document.querySelector('button');
+
+const markerIcon = L.icon({
+  iconUrl: icon,
+  iconSize: [30, 40],
+});
 
 const inInfo = document.querySelector('#ip');
 const locationInfo = document.querySelector('#location');
@@ -10,6 +18,15 @@ const ispInfo = document.querySelector('#isp');
 
 btn.addEventListener('click', getData);
 ipInput.addEventListener('keydown', handleKey);
+
+const mapArea = document.querySelector('.map');
+const map = L.map(mapArea, {
+  center: [51.505, -0.09],
+  zoom: 13,
+  zoomControl: false,
+});
+addTileLayer(map);
+L.marker([51.505, -0.09], { icon: markerIcon }).addTo(map);
 
 function getData() {
   if (validateIp(ipInput.value)) {
@@ -28,9 +45,9 @@ function handleKey(e) {
 }
 
 function setInfo(mapData) {
-    console.log(mapData);
-    inInfo.innerText = mapData.ip;
-    locationInfo.innerText = mapData.location.country + ', ' + mapData.location.region;
-    timezoneInfo.innerText = mapData.location.timezone;
-    ispInfo.innerText = mapData.isp;
+  console.log(mapData);
+  inInfo.innerText = mapData.ip;
+  locationInfo.innerText = mapData.location.country + ', ' + mapData.location.region;
+  timezoneInfo.innerText = mapData.location.timezone;
+  ispInfo.innerText = mapData.isp;
 }
